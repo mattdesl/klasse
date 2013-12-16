ecef9391859a0e46633e644d8bf8f2b03d425f44
mattdesl <dave.des@gmail.com>
2013-12-15 21:47:23 -0500

    adding UMD build

--- build/klasse.js
+++ build/klasse.js
@@ -1,3 +1,5 @@
+!function(e){"object"==typeof exports?module.exports=e():"function"==typeof define&&define.amd?define(e):"undefined"!=typeof window?window.klasse=e():"undefined"!=typeof global?global.klasse=e():"undefined"!=typeof self&&(self.klasse=e())}(function(){var define,module,exports;
+return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 function hasGetterOrSetter(def) {
 	return (!!def.get && typeof def.get === "function") || (!!def.set && typeof def.set === "function");
 }
@@ -180,3 +182,7 @@ Class.mixin = mixin;
 Class.ignoreFinals = false;
 
 module.exports = Class;
+},{}]},{},[1])
+(1)
+});
+;
